a93e61e7d274a6cf8db9d57379a3c57a7f7dda5b
theodpzz <101199304+theodpzz@users.noreply.github.com>
2025-03-11 11:05:48 +0100

    Create convnextv2.py

---
+++ step1/src/models/convnextv2.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from transformers import ConvNextV2ForImageClassification
+from .modules.loss import CombinedLoss
+
+class ConvNeXtV2Backbone(nn.Module):
+    def __init__(self, args):
+        super(ConvNeXtV2Backbone, self).__init__()
+        
+        self.args = args
+
+        # Load ConvNeXt pretrained features
+        self.features = ConvNextV2ForImageClassification.from_pretrained(args.path_convnext).convnextv2
+
+    def forward(self, images):
+
+        # extract features with resnet
+        x = self.features(images)
+
+        # global average pooling
+        x = x.last_hidden_state.mean(dim=(2, 3))
+
+        return x
